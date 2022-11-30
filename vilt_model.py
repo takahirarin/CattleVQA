30b22010858c53e4ecf3ef90dc9f77921186361d
takahirarin <takahira@cs25.scitec.kobe-u.ac.jp>
2022-11-30 17:54:33 +0900

    embedding部分作成

--- vilt_model.py
+++ vilt_model.py
@@ -6,10 +6,12 @@ from img_preprocess import PatchEmbeddings
 from vilt_config import ViltConfig
 import copy
 from transformers.modeling_utils import  ModuleUtilsMixin
-from transformer_block import MultiHeadAttention
+from transformer_block import MultiHeadAttention,FFN,SubLayerConnection
 
 from transformers import ViltProcessor
-from dataset import VQADataset, read_data
+from dataset import VQADataset, read_data, collate_fn
+from torch.utils.data import DataLoader
+from tqdm.notebook import tqdm
 
 class ViltModel(nn.Module):
     def __init__(self, config: ViltConfig):
@@ -40,8 +42,8 @@ class ViltModel(nn.Module):
             pixel_values, pixel_mask,image_token_type_idx )
         
         # input embeddings into encoder
-        extended_attention_mask = ModuleUtilsMixin.get_extended_attention_mask(attention_mask, input_shape)
-        encoder_output = self.encoder(embeddings, extended_attention_mask)
+        #extended_attention_mask = ModuleUtilsMixin.get_extended_attention_mask(attention_mask, input_shape)
+        encoder_output = self.encoder(embeddings, attention_mask)
         sequence_output = encoder_output[-1]
         pooled_output = self.pooler(sequence_output)
 
@@ -55,8 +57,8 @@ class ViltModel(nn.Module):
 
 class ViltClassifer(nn.Module):
     def __init__(self, config):
-        super(ViltClassifer).__init__()
-        self.fc = nn.linear(config.hidden_size, config.hidden_size*2)
+        super().__init__()
+        self.fc = nn.Linear(config.hidden_size, config.hidden_size*2)
         self.norm = nn.LayerNorm(config.hidden_size*2)
         self.activation = config.hidden_act
 
@@ -80,25 +82,107 @@ class ViltEmbeddings(nn.Module):
         # patch embeddings
         self.patch_embeddings = PatchEmbeddings(config)
         self.cls_token = nn.Parameter(torch.zeros(1, 1, config.hidden_size))
-
-        image_size, patch_size = config.image_size, config.patch_size
-        num_patches = (image_size[1] // patch_size[1]) * (image_size[0] // patch_size[0])
+        num_patches = self.patch_embeddings.num_patches
         
         # num_patches = config.num_patches
-        self.position_embeddings = nn.Paramete(torch.zeros(1, num_patches + 1, config.hidden_size))
+        self.position_embeddings = nn.Parameter(torch.zeros(1, num_patches + 1, config.hidden_size))
         # modality type embedding
         self.token_type_embeddings = nn.Embedding(config.modality_type_vocab_size, config.hidden_size)
         self.dropout = nn.Dropout(config.hidden_dropout_prob)
         self.config = config
-        # self.fc = nn.
 
-        def visual_embed(self,pixel_values, pixel_mask, mask_image_length=200):
-            """ """
-            pass
+    def visual_embed(self,pixel_values, pixel_mask, max_image_length=200):
+        _, _, ph, pw = self.patch_embeddings.projection.weight.shape
+        print(pixel_values.size)
+        x = self.patch_embeddings(pixel_values)
+        x_mask = pixel_mask[:, None, :, :].float()
+        x_mask = nn.functional.interpolate(x_mask, size=(x.shape[2], x.shape[3])).long()
+        x_h = x_mask[:, 0].sum(dim=1)[:, 0]
+        x_w = x_mask[:, 0].sum(dim=2)[:, 0]
 
-        def forward(self, 
-                        input_ids, attention_mask, token_type_ids,
-                         pixel_values, pixel_mask,image_token_type_idx=1):
+        batch_size, num_channels, height, width = x.shape
+        patch_dim = self.config.image_size // self.config.patch_size
+        spatial_pos = self.position_embeddings[:, 1:, :].transpose(1, 2).view(1, num_channels, patch_dim, patch_dim)
+        pos_embed = torch.cat(
+            [
+                nn.functional.pad(
+                    nn.functional.interpolate(
+                        spatial_pos,
+                        size=(h, w),
+                        mode="bilinear",
+                        align_corners=True,
+                    ),
+                    (0, width - w, 0, height - h),
+                )
+                for h, w in zip(x_h, x_w)
+            ],
+            dim=0,
+        )
+
+        pos_embed = pos_embed.flatten(2).transpose(1, 2)
+        x = x.flatten(2).transpose(1, 2)
+         # Set `device` here, otherwise `patch_index` will always be on `CPU` and will fail near the end for torch>=1.13
+        patch_index = torch.stack(
+            torch.meshgrid(torch.arange(x_mask.shape[-2]), torch.arange(x_mask.shape[-1]), indexing="ij"), dim=-1
+        ).to(device=x_mask.device)
+        patch_index = patch_index[None, None, :, :, :]
+        patch_index = patch_index.expand(x_mask.shape[0], x_mask.shape[1], -1, -1, -1)
+        patch_index = patch_index.flatten(1, 3)
+        x_mask = x_mask.flatten(1)
+
+        if max_image_length < 0 or max_image_length is None or not isinstance(max_image_length, int):
+            # suppose aug is 800 x 1333, then, maximum effective res is 800 x 1333 (if one side gets bigger, the other will be constrained and be shrinked)
+            # (800 // self.patch_size) * (1333 // self.patch_size) is the maximum number of patches that single image can get.
+            # if self.patch_size = 32, 25 * 41 = 1025
+            # if res is 384 x 640, 12 * 20 = 240
+            effective_resolution = x_h * x_w
+            max_image_length = effective_resolution.max()
+        else:
+            effective_resolution = x_h * x_w
+            max_image_length = min(effective_resolution.max(), max_image_length)
+
+        valid_idx = x_mask.nonzero(as_tuple=False)
+        non_valid_idx = (1 - x_mask).nonzero(as_tuple=False)
+        unique_rows = valid_idx[:, 0].unique()
+        valid_row_idx = [valid_idx[valid_idx[:, 0] == u] for u in unique_rows]
+        non_valid_row_idx = [non_valid_idx[non_valid_idx[:, 0] == u] for u in unique_rows]
+        valid_nums = [v.size(0) for v in valid_row_idx]
+        non_valid_nums = [v.size(0) for v in non_valid_row_idx]
+        pad_nums = [max_image_length - v for v in valid_nums]
+
+        select = list()
+        for i, (v, nv, p) in enumerate(zip(valid_nums, non_valid_nums, pad_nums)):
+            if p <= 0:
+                valid_choice = torch.multinomial(torch.ones(v).float(), max_image_length)
+                select.append(valid_row_idx[i][valid_choice])
+            else:
+                pad_choice = torch.multinomial(torch.ones(nv).float(), p, replacement=True)
+                select.append(torch.cat([valid_row_idx[i], non_valid_row_idx[i][pad_choice]], dim=0))
+
+        select = torch.cat(select, dim=0)
+        x = x[select[:, 0], select[:, 1]].view(batch_size, -1, num_channels)
+        x_mask = x_mask[select[:, 0], select[:, 1]].view(batch_size, -1)
+        # `patch_index` should be on the same device as `select` (for torch>=1.13), which is ensured at definition time.
+        patch_index = patch_index[select[:, 0], select[:, 1]].view(batch_size, -1, 2)
+        pos_embed = pos_embed[select[:, 0], select[:, 1]].view(batch_size, -1, num_channels)
+
+        cls_tokens = self.cls_token.expand(batch_size, -1, -1)
+        x = torch.cat((cls_tokens, x), dim=1)
+        pos_embed = torch.cat(
+            (self.position_embeddings[:, 0, :][:, None, :].expand(batch_size, -1, -1), pos_embed), dim=1
+        )
+        x = x + pos_embed
+        x = self.dropout(x)
+
+        x_mask = torch.cat([torch.ones(x_mask.shape[0], 1).to(x_mask), x_mask], dim=1)
+
+        return x, x_mask, (patch_index, (height, width))
+
+
+
+
+    def forward(self,input_ids, attention_mask, token_type_ids,
+                    pixel_values, pixel_mask,image_token_type_idx=1):
             # 1. text embeddings
             text_embeds = self.text_embeddings(
                 input_ids = input_ids, token_type_ids= token_type_ids )
@@ -108,15 +192,27 @@ class ViltEmbeddings(nn.Module):
             for example: 
                 import clip
                 model, preprocess = clip.load("ViT-B/32", device=device)
-                image = preprocess(Image.open("CLIP.png")).unsqueeze(0).to(device)
-                image_embeds = model.encode_image(image)
+                from transformers import CLIPVisionModel
+
+                model = CLIPVisionModel.from_pretrained("openai/clip-vit-base-patch32")
+                
+                # inputs = processor(images=image, return_tensors="pt")
+
+                outputs = model(**inputs)
+                last_hidden_state = outputs.last_hidden_state
+                pooled_output = outputs.pooler_output  # pooled CLS states
+
+
+
+
+
             """
-            image_embeds, image_masks, patch_index = image_embeds(
-                pixel_values , pixel_mask, max_image_length=self.config.max_iamge_length )
+            image_embeds, image_masks, patch_index = self.visual_embed(
+                pixel_values , pixel_mask, max_image_length=self.config.max_image_length )
             
             # 3. add modality type embedding
-            text_embeds = text_embeds + self.token_type_embeddings(
-                torch.zeros_like(attention_mask,dtype=torch.long, device = text_embeds.device))
+            # text_embeds = text_embeds + self.token_type_embeddings(
+            #     torch.zeros_like(attention_mask,dtype=torch.long, device = text_embeds.device))
 
             image_embeds = image_embeds + self.token_type_embeddings(
                 torch.full_like(image_masks, image_token_type_idx, dtype=torch.long, device=image_embeds.device))
@@ -124,13 +220,12 @@ class ViltEmbeddings(nn.Module):
             # 4. concat
             embeddings = torch.cat([text_embeds, image_embeds], dim =1)
             masks = torch.cat([attention_mask, image_masks], dim=1)
-
             return embeddings, masks
 
 
 class ViltEncoder(nn.Module):
     def __init__(self,config) :
-        super(ViltEncoder).__init__()
+        super().__init__()
         self.config = config
         layer = ViltLayer(config)
         self.layer = nn.ModuleList([copy.deepcopy(layer) for _ in range(config.num_hidden_layers)])
@@ -146,13 +241,20 @@ class ViltEncoder(nn.Module):
 
 class ViltLayer(nn.Module):
     def __init__(self, config):
-        super(ViltLayer).__init__()
+        super().__init__()
         self.attention = MultiHeadAttention(config)
+        self.transformer_block = FFN(config)
+        self.shortcut = SubLayerConnection(config)
+    def forward(self,hidden_states,attention_mask):
+        attention_output = self.attention(hidden_states)
+        FFN_output = self.transformer_block(attention_output)
+        layer_output = self.shortcut(FFN_output, attention_output)
+        return layer_output
 
 
 class ViltPooler(nn.Module):
     def __init__(self,config):
-        super(ViltPooler).__init__()
+        super().__init__()
         self.dense = nn.Linear(config.hidden_size, config.hidden_size)
         self.activation = nn.Tanh()
     
@@ -165,14 +267,19 @@ class ViltPooler(nn.Module):
 
 if __name__ == "__main__":
     config = ViltConfig()
-    print(config.vocab_size)
-    model = ViltModel(config)
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model = ViltModel(config).to(device)
     path1 = 'Dataset/questions/v2_OpenEnded_mscoco_val2014_questions.json'
     path2 = 'Dataset/annotations/v2_mscoco_val2014_annotations.json'
     questions, annotations = read_data(path1,path2,config)
     processor = ViltProcessor.from_pretrained("dandelin/vilt-b32-mlm")
-    dataset = VQADataset( questions, annotations,processor,config)
+    dataset = VQADataset( questions, annotations,config)
     
-    encoder_output, pooled_output, output = model(*dataset)
-    print(encoder_output, pooled_output, output)
+    dataloader = DataLoader(dataset, collate_fn=collate_fn,batch_size =config.batch_size,shuffle=True )
+    for datas in tqdm(dataloader):
+        data = {k:v.to(device) for k,v in datas.items()}
+        data.pop('labels')
+        print(data.keys())
+        encoder_output, pooled_output, output = model(**data)
+        print(encoder_output, pooled_output, output)
 
